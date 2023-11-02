deba73116c9e8064699eca5a0e6353bf5bd4fe97
Nayankyadav <144725447+Nayankyadav@users.noreply.github.com>
2023-11-02 20:18:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const byteSize = (str) => {
+	function byteSize(str) {
+  // Create a Blob object from the input string
+  const blob = new Blob([str]);
+
+  // Get the size of the Blob in bytes
+  return blob.size;
+}
+
+// Test cases
+console.log(byteSize('hello world'));     // Output: 11
+console.log(byteSize('안녕하세요'));      // Output: 15
+console.log(byteSize(''));                // Output: 0
+
   // write your code here
 };
 
